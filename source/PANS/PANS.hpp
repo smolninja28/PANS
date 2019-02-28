8bbf9a00b3f09615d39601e793d6f15c2524cc26
0X4B-Developer <40446335+0X4B-Developer@users.noreply.github.com>
2019-02-27 17:36:17 -0700

    Setup namespaces

--- source/PANS/PANS.hpp
+++ source/PANS/PANS.hpp
@@ -1,1 +1,20 @@
 #pragma once
+
+namespace PANS
+{
+  //FUNCTIONALITY DEFINES//
+  #define ConfigScreen //pre-game initialization robot configuration screen module
+
+  //core data
+  enum class ReturnResult_e_t
+  {
+    Success,
+    GenericFailure
+  };
+
+  namespace Core
+  {
+    //initialization
+    ReturnResult_e_t Initialize();
+  }
+}
